e6113031ee4b7820733851761abc88d8e76f29cf
Leo Veres <leoveres@gmail.com>
2018-05-10 16:47:49 -0400

    did it

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+/* eslint-env jquery */
+//input textbox and hit add item creates li
+//span=== ${input value}
+
+$(function(){
+  
+  $('#js-shopping-list-form').submit(function(event){
+
+    event.preventDefault();
+
+    const newItem = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    $('.shopping-list').append(
+      `
+      <li>
+      <span class="shopping-item">${newItem}</span>
+      <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+      </div>
+    </li>
+    `
+    );
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event){
+    this.closest('li').remove();
+  });
+console.log('5');
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+});
+
+
+
+
+
+
+
